c40609e256f8b637fb99ec438ca8d10903715fcf
MajdSoubh <majdsoubh53@gmail.com>
2024-12-30 18:38:16 +0300

    install tailwindcss and organize the structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { Navbar } from "./components/Navbar";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="bg-black-100">
+      <Navbar />
     </div>
   );
 }
