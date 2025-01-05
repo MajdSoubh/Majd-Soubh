45391fac48a7dd2523565c82c035283a1ac7d842
MajdSoubh <majdsoubh53@gmail.com>
2025-01-05 13:49:48 +0300

    [Fix] handle unmount case

--- src/hooks/useTypeWriter.jsx
+++ src/hooks/useTypeWriter.jsx
@@ -3,41 +3,49 @@ import { useEffect, useState } from "react";
 export const useTypeWriter = (texts, speed, infinity = false, loop = false) => {
   const [displayText, setDisplayText] = useState("");
 
-  const animateTyping = async (texts) => {
-    const animate = async function (text) {
-      return new Promise((resolve) => {
-        let charIndex = 0;
-        let reverseCharIndex = loop ? text.length : 0;
+  useEffect(() => {
+    let isMounted = true; // Track if the component is still mounted
+    let typingInterval;
 
-        const typingInterval = setInterval(() => {
-          if (charIndex <= text.length) {
-            setDisplayText(text.slice(0, charIndex));
-            charIndex++;
-          } else if (reverseCharIndex > 0) {
-            setDisplayText(text.slice(0, reverseCharIndex));
-            reverseCharIndex--;
-          } else {
-            resolve();
-            clearInterval(typingInterval);
-          }
-        }, speed);
-      });
+    const animateTyping = async (texts) => {
+      const animate = (text) => {
+        return new Promise((resolve) => {
+          let charIndex = 0;
+          let reverseCharIndex = loop ? text.length : 0;
+
+          typingInterval = setInterval(() => {
+            if (charIndex <= text.length) {
+              setDisplayText(text.slice(0, charIndex));
+              charIndex++;
+            } else if (reverseCharIndex > 0) {
+              setDisplayText(text.slice(0, reverseCharIndex));
+              reverseCharIndex--;
+            } else {
+              clearInterval(typingInterval);
+              resolve();
+            }
+          }, speed);
+        });
+      };
+
+      do {
+        for (const text of texts) {
+          if (!isMounted) break; // Stop if the component is unmounted
+          await animate(text);
+        }
+      } while (infinity && isMounted);
     };
 
-    do {
-      for (const text of texts) {
-        await animate(text);
-      }
-    } while (infinity * 100);
-  };
-
-  useEffect(() => {
     const normalizedTexts = Array.isArray(texts) ? texts : [texts];
-
     animateTyping(normalizedTexts);
 
-    return () => setDisplayText("");
-  }, [speed, texts]);
+    // Cleanup function
+    return () => {
+      isMounted = false;
+      clearInterval(typingInterval);
+      setDisplayText("");
+    };
+  }, [texts, speed, infinity, loop]);
 
   return displayText;
 };
