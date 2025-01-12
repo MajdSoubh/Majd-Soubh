23b3d5a641522790f0d3ee0d8e431fc7ed3c9966
MajdSoubh <majdsoubh53@gmail.com>
2025-01-12 14:32:37 +0300

    [WIP] allow to extend it's classes

--- src/components/SpotLight.jsx
+++ src/components/SpotLight.jsx
@@ -6,11 +6,14 @@ export const SpotLight = ({
   top = null,
   right = null,
   bottom = null,
+  className = "",
 }) => {
   return (
     <div
       className={
-        "overflow-hidden blur-[10rem] absolute w-[18rem] h-[18rem] z-0 bg-clip-border" +
+        "overflow-hidden blur-[10rem]  absolute w-[16rem] h-[16rem] sm:w-[18rem] sm:h-[18rem] z-0 bg-clip-content " +
+        className +
+        " " +
         (left ? "-translate-x-1/2 " : "translate-x-1/2 ") +
         (top ? "-translate-y-1/2" : "translate-y-1/2")
       }
