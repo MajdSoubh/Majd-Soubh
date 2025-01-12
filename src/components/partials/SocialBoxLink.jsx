59dfc1551a185a56f13aee39238fa043509d342e
MajdSoubh <majdsoubh53@gmail.com>
2025-01-12 14:32:10 +0300

    [FIX] background color and moving event

--- src/components/partials/SocialBoxLink.jsx
+++ src/components/partials/SocialBoxLink.jsx
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export const SocialBoxLink = ({
   children,
-  hoverColor = "black",
+  hoverColor = "rgba(255, 255, 255, 0.125)",
   link = "#",
 }) => {
+  const wrapperRef = useRef();
+  const handleHover = (e) => {
+    wrapperRef.current.style.backgroundColor = hoverColor;
+  };
+
+  const handleLeave = (e) => {
+    wrapperRef.current.style.backgroundColor = "hsla(0, 3%, 30%, 0.477)";
+  };
   return (
-    <a href={link} target="_blank">
-      <div className="flex items-center justify-center transition-transform duration-300 ease-in-out hover:-translate-y-4  w-16 h-16 group cursor-pointer">
+    <a href={link} target="_blank" rel="noreferrer">
+      <div
+        className="flex items-center justify-center w-16 h-16 cursor-pointer group"
+        onMouseEnter={handleHover}
+        onMouseLeave={handleLeave}
+        onTouchStart={handleHover}
+        onTouchEnd={handleLeave}
+      >
         {/* Wrapper */}
         <div
-          className={`relative bg-black-200 w-12 h-12 shadow-md perspective-500 transform-style-3d origin-center rotate-x-45 -rotate-z-45 group-hover:-rotate-z-0 transition-transform duration-300 ease-in-out group-hover:bg-${hoverColor}-700`}
+          ref={wrapperRef}
+          className="relative bg-[hsla(0,3%,30%,0.477)] w-11 h-11 perspective-800 transform-style-3d transform  rotate-x-45 -rotate-z-45 transition-all duration-300 ease-in-out group-hover:translate-y-[-0.6rem]"
         >
           {/* Front Face */}
-          <div className="absolute top-0 left-0 border border-black-300 bg-inherit p-2 w-12 h-12 flex items-center justify-center">
+          <div className="absolute top-0 left-0 border border-white/20 bg-inherit p-2 w-11 h-11 flex items-center justify-center">
             {children}
           </div>
           {/* Side Faces */}
-          <div className="absolute bg-inherit border border-black-300 top-0 left-0 w-4 h-12 origin-left rotate-y-90 "></div>
-          <div className="absolute bg-inherit border border-black-300 left-0 bottom-0 h-4 w-12 origin-bottom rotate-x-90"></div>
+          <div className="absolute bg-inherit border border-white/20 top-0 left-0 w-4 h-11 origin-left transform rotate-x-0 rotate-y-90 "></div>
+          <div className="absolute bg-inherit border border-white/20 left-0 bottom-0 h-4 w-11 origin-bottom transform rotate-y-0 rotate-x-90"></div>
         </div>
       </div>
     </a>
