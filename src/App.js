e1a69d96e56d5afaad93ebc0f82a4f75f147aa36
MajdSoubh <majdsoubh53@gmail.com>
2025-01-15 16:32:05 +0300

    [WIP] Changed the layout of the page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import About from "./components/About";
 import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
-import { SpotLight } from "./components/SpotLight";
-
+import LiIcon from "./components/partials/LiIcon";
+import Timeline from "./components/partials/Timeline";
 function App() {
   return (
-    <div className="bg-black text-white overflow-hidden">
-      <SpotLight bgColor={"#4ade80"} left={"0px"} top={"0px"} />
-      <SpotLight bgColor={"#ff00ff"} right={"0px"} top={"600px"} />
-      <div className="z-10 relative">
-        <Navbar />
-        <Home />
+    <div className="bg-black relative text-white overflow-hidden px-4 mx-auto">
+      <Navbar />
+      <Home />
+
+      <div className="relative  my-32 bg-dark text-white ">
+        <Timeline>
+          <About />
+          <About />
+          <About />
+          <About />
+          <LiIcon className={"absolute left-8"} />
+          <About />
+          <About />
+          <About />
+          <About />
+          <About />
+          <About />
+        </Timeline>
       </div>
     </div>
   );
