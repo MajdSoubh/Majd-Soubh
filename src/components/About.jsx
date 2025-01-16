f6b8a12b8fdca158f72236823a08cc78219e7291
MajdSoubh <majdsoubh53@gmail.com>
2025-01-16 18:41:47 +0300

    [STYLE] Enhance the about style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,16 +2,19 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="w-[min(100%,500px)] p-2">
-      <h2 className="text-4xl text-slate-100 sm:text-[2.7rem] transition-all w-fit after:content-[''] after:bg-slate-600/45 after:h-[2px] sm:after:w-64 after:relative after:-bottom-1 after:left-0 after:block mb-4">
-        About Me
-      </h2>
-      <p className="text-base text-slate-primary">
-        Self-taught Full-stack Developer with over two years of hands-on
-        experience, driven by a passion for continuous learning and utilizing
-        diverse cutting-edge technologies in the job market to create innovative
-        projects.
-      </p>
+    <div className="relative w-[min(100%,500px)] p-2 mx-auto ">
+      <div className="flex flex-col items-center">
+        <h2 className="text-4xl text-slate-100 sm:text-[2.7rem] transition-all w-fit ">
+          About Me
+        </h2>
+        <p className="text-base text-slate-primary text-center mt-4">
+          Self-taught Full-stack Developer with over two years of hands-on
+          experience, driven by a passion for continuous learning and utilizing
+          diverse cutting-edge technologies in the job market to create
+          innovative projects.
+        </p>
+      </div>
+      <div className="mt-6 mx-auto w-[min(500px,60%)] bg-slate-600/45 h-[2px]" />
     </div>
   );
 }
