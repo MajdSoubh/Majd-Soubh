ccf41f4630392d32d215c153b0bd2dca0ba160e8
MajdSoubh <majdsoubh53@gmail.com>
2025-01-16 18:43:21 +0300

    [WIP] allow user to update the blur value

--- src/components/SpotLight.jsx
+++ src/components/SpotLight.jsx
@@ -1,23 +1,33 @@
 import React from "react";
 
-export const SpotLight = ({
+const SpotLight = ({
   bgColor,
   left = null,
   top = null,
   right = null,
   bottom = null,
+  blur = "10rem",
   className = "",
 }) => {
   return (
     <div
       className={
-        "overflow-hidden blur-[10rem]  absolute w-[16rem] h-[16rem] sm:w-[18rem] sm:h-[18rem] z-0 bg-clip-content " +
+        "overflow-hidden blur absolute w-[16rem] h-[16rem] sm:w-[18rem] sm:h-[18rem] z-0 bg-clip-content " +
         className +
         " " +
         (left ? "-translate-x-1/2 " : "translate-x-1/2 ") +
         (top ? "-translate-y-1/2" : "translate-y-1/2")
       }
-      style={{ backgroundColor: bgColor, left, top, right, bottom }}
+      style={{
+        filter: `blur(${blur})`,
+        backgroundColor: bgColor,
+        left,
+        top,
+        right,
+        bottom,
+      }}
     ></div>
   );
 };
+
+export default SpotLight;
