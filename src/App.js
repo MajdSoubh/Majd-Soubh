76e6e24a5f25d61c4aebc5bb7aced9c1105b5b9c
MajdSoubh <majdsoubh53@gmail.com>
2025-01-17 21:55:33 +0300

    [WIP] update content

--- src/App.js
+++ src/App.js
@@ -31,19 +31,19 @@ function App() {
         </div>
 
         {/* Experince & Education */}
-        <div className="mt-16 space-y-8">
+        <div className="mt-16 space-y-10">
           {/* Timeline Switcher */}
           <div className="mx-auto flex justify-center items-center gap-8">
             <button
               onClick={() => setShowWorkDetails(true)}
-              className={`font-bold flex items-center py-2 px-3 gap-4 text-xl tracking-wide  transition-all ${
+              className={`font-bold flex items-center py-2 px-3 gap-2 text-md tracking-wide  transition-all ${
                 showWorkDetails
-                  ? " rounded-md  bg-emerald-800/80 text-white "
+                  ? " rounded-md  bg-turquoise/50 text-white "
                   : "text-gray-500 "
               }`}
             >
               <WorkIcon
-                className={` fill-gray-500 !w-8 ${
+                className={` fill-gray-500 !size-6 ${
                   showWorkDetails ? " !fill-white " : " "
                 }`}
               />
@@ -52,14 +52,14 @@ function App() {
 
             <button
               onClick={() => setShowWorkDetails(false)}
-              className={`font-bold items-center flex py-2 px-3 gap-4 text-xl tracking-wide  transition-all ${
+              className={`font-bold items-center flex py-2 px-3 gap-2 text-md tracking-wide  transition-all ${
                 !showWorkDetails
-                  ? "  rounded-md bg-emerald-800/80 text-white "
+                  ? "  rounded-md bg-turquoise/50 text-white "
                   : "text-gray-500 "
               }`}
             >
               <AcademicIcon
-                className={` fill-gray-500  !w-8 ${
+                className={` fill-gray-500  !size-6  ${
                   !showWorkDetails ? " !fill-white " : ""
                 }`}
               />
@@ -67,53 +67,52 @@ function App() {
             </button>
           </div>
 
-          {showWorkDetails && (
-            <Timeline>
-              <WorkDetials
-                position="Software Engineer"
-                companyLink="http=//intenta.de"
-                company="LIT"
-                time="Nov 2023 - Jun 2024"
-                address="Lebanon, Remote"
-                work="LIT, is a subsidiary of Intenta GmbH in Germany, focuses on developing smart sensors and car driving assistant products."
-                accomplishments={[
-                  "Collaborating with cross-functional teams to optimize localization algorithms, resulting in a 25% increase in availability and ensuring continuous service for end-users",
-                  "Refactored 6+ key components, improving code efficiency by 30%",
-                  "Generating automated tests for 5 critical features, resulting in improved feature stability",
-                  "Performed code reviews, ensuring high-quality code and consistent development standards",
-                ]}
-              />
+          {/* Time Line Box */}
+          <div>
+            {showWorkDetails && (
+              <Timeline dateRanges={["Nov 2023 - Jun 2024", "2021-Present"]}>
+                <WorkDetials
+                  position="Software Engineer"
+                  companyLink="https://intenta.de"
+                  company="Lebanon Information Technology"
+                  address="Lebanon, Remote"
+                  work="LIT, is a subsidiary of Intenta GmbH in Germany, focuses on developing smart sensors and car driving assistant products."
+                  accomplishments={[
+                    "Collaborating with cross-functional teams to optimize localization algorithms, resulting in a 25% increase in availability and ensuring continuous service for end-users",
+                    "Refactored 6+ key components, improving code efficiency by 30%",
+                    "Generating automated tests for 5 critical features, resulting in improved feature stability",
+                    "Performed code reviews, ensuring high-quality code and consistent development standards",
+                  ]}
+                />
 
-              <WorkDetials
-                position="Full-Stack Developer"
-                company="Freelancer"
-                time="2021-Present"
-                address="Remote"
-                work="Launched custom web applications for 4 clients, leveraging PHP, Laravel, React and Vue.js to meet their needs."
-                accomplishments={[
-                  "Worked on planning, designing, developing and testing small to mid-scaled projects",
-                  "Continuous maintainance for the systems",
-                ]}
-              />
-            </Timeline>
-          )}
-          {!showWorkDetails && (
-            <Timeline>
-              <EducationDetails
-                type="Bachelor Of Information Technology"
-                time="2018-2023"
-                place="Tishreen University"
-                info="Relevant courses included Software Engineering, Data Structures and Algorithms, Database Management Systems, Web Development and Artificial Intelligence."
-              />
+                <WorkDetials
+                  position="Full-Stack Developer"
+                  company="Freelancer"
+                  address="Remote"
+                  work="Launched custom web applications for 4 clients, leveraging PHP, Laravel, React and Vue.js to meet their needs."
+                  accomplishments={[
+                    "Worked on planning, designing, developing and testing small to mid-scaled projects",
+                    "Continuous maintainance for the systems",
+                  ]}
+                />
+              </Timeline>
+            )}
+            {!showWorkDetails && (
+              <Timeline dateRanges={["2018-2023", "2023-present"]}>
+                <EducationDetails
+                  type="Bachelor Of Information Technology"
+                  place="Tishreen University"
+                  info="Relevant courses included Software Engineering, Data Structures and Algorithms, Database Management Systems, Web Development and Artificial Intelligence."
+                />
 
-              <EducationDetails
-                type="Master Web Science"
-                time="2023-present"
-                place="Syrian Virtual University"
-                info="This program offers advanced expertise in web technologies and internet systems, covering topics like web development, data mining, analysis, security, and cloud computing"
-              />
-            </Timeline>
-          )}
+                <EducationDetails
+                  type="Master Web Science"
+                  place="Syrian Virtual University"
+                  info="This program offers advanced expertise in web technologies and internet systems, covering topics like web development, data mining, analysis, security, and cloud computing"
+                />
+              </Timeline>
+            )}
+          </div>
         </div>
       </div>
     </div>
